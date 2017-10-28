b8eb7de9ef9453f7a71495b5f73b7f42f304f0be
QuirinEcker <quirin.ecker@bajupa.com>
2017-10-28 19:03:55 +0200

    deklariere Variablen

--- src/main/java/Ecker_Wechselgeld.java
+++ src/main/java/Ecker_Wechselgeld.java
@@ -1,2 +1,34 @@
+import java.util.Scanner;
+
 public class Ecker_Wechselgeld {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        final int EINEUR = 100;
+        final int FÜNFZIGCENT = 50;
+        final int ZWANZIGCENT = 20;
+        final int ZEHNCENT = 10;
+        final int FÜNFCENT = 5;
+        final int ZWEICENT = 2;
+        final int EINCENT = 1;
+        int eineur;
+        int fünfzigcent;
+        int zwanzigcent;
+        int zehncent;
+        int fünfcent;
+        int zweicent;
+        int eincent;
+        int eingabe;
+
+        System.out.print("Geben sie bitte einen Betrag ein (zwischen 0 und 100 Cent)");
+        eingabe = scanner.nextInt();
+
+        eineur = eingabe / EINEUR;
+
+
+
+
+
+    }
 }
